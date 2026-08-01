b0c5824b24172ec94c5c5ca6ede762f3fa22520c
plekkocoin <Plekkocoin@gmail.com>
2026-08-01 09:08:54 +0330

    Replace 3D model with Plekko image

--- app.js
+++ app.js
@@ -1,75 +1,11 @@
-import * as THREE from 
-"https://unpkg.com/three@0.160.0/build/three.module.js";
+const fish = document.createElement("img");
 
-import { GLTFLoader } from 
-"https://unpkg.com/three@0.160.0/examples/jsm/loaders/GLTFLoader.js";
+fish.src = "plekko.png";
+fish.style.width = "250px";
+fish.style.maxWidth = "80%";
+fish.style.display = "block";
+fish.style.margin = "0 auto";
+fish.style.userSelect = "none";
+fish.draggable = false;
 
-
-const scene = new THREE.Scene();
-
-const camera = new THREE.PerspectiveCamera(
-45,
-window.innerWidth / window.innerHeight,
-0.1,
-1000
-);
-
-const renderer = new THREE.WebGLRenderer({
-alpha:true,
-antialias:true
-});
-
-renderer.setSize(
-window.innerWidth,
-350
-);
-
-document
-.getElementById("fish3d")
-.appendChild(renderer.domElement);
-
-
-const light = new THREE.HemisphereLight(
-0xffffff,
-0x004466,
-3
-);
-
-scene.add(light);
-
-
-const loader = new GLTFLoader();
-
-loader.load(
-"models/plekko.glb",
-function(gltf){
-
-const fish = gltf.scene;
-
-fish.scale.set(
-1.5,
-1.5,
-1.5
-);
-
-scene.add(fish);
-
-animate();
-
-}
-);
-
-
-camera.position.z = 5;
-
-
-function animate(){
-
-requestAnimationFrame(animate);
-
-renderer.render(
-scene,
-camera
-);
-
-}
+document.getElementById("fish3d").appendChild(fish);
